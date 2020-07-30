fe97de04a5defe637ec12da0603c217388b0ac25
Navid Khalili <nkhalili@live.com>
2020-07-30 10:15:34 -0400

    init spring

---
+++ src/main/java/com/example/spring2demo/Spring2demoApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring2demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring2demoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring2demoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring2demo/Spring2demoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring2demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring2demoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
